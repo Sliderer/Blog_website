e803dfa319a4c7d83a5a35ebc5a917bd7f5593b3
Sliderer <a6271547@mail.ru>
2023-02-21 23:48:35 +0300

    test

--- Website/main.py
+++ Website/main.py
@@ -4,6 +4,7 @@ from secret_key_generator import SecretKeyGenerator
 app = Flask(__name__)
 
 if __name__ == '__main__':
+    print('hello')
     secret_key = SecretKeyGenerator.generate_secret_code()
     app.secret_key = secret_key
     app.run(debug=True)
